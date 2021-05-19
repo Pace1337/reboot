98e9ae83264072c6405c14822bceee979a053092
Pace <pacex3123@gmail.com>
2021-05-19 10:03:37 +0200

    Full source code

---
+++ src/main/java/dev/pace/reboot/Reboot.java
@@ -0,0 +1,54 @@
+package dev.pace.reboot;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public final class Reboot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("Plugin successfully enabled.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            if (cmd.getName().equalsIgnoreCase("reboot")) {
+
+                if (!sender.hasPermission("reboot.use")){
+                    sender.sendMessage("You have no permission to execute this command.");
+                    return true;
+                }
+
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "bc &b&lThe server will reboot in 60 seconds!");
+                new BukkitRunnable() {
+                    int counter = 60;
+
+                    public void run() {
+                        counter--;
+                        if (counter == 50 || counter == 40 || counter == 30 || counter == 20 || counter == 10 || counter == 1) {
+                            Bukkit.broadcastMessage("Server will reboot in " + counter + " seconds!");
+                        }
+
+                        if (counter == 0) {
+
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "restart");
+                        }
+                    }
+                }.runTaskTimer(this, 20, 20);
+            }
+
+        }
+        return true;
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("Plugin disabled.");
+
+    }
+}
