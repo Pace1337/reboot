d3ae045a53ddb1e290d8109080a25908a1aa8559
Pace <pacex3123@gmail.com>
2021-05-20 10:49:58 +0200

    Organized code

--- src/main/java/dev/pace/reboot/Reboot.java
+++ src/main/java/dev/pace/reboot/Reboot.java
@@ -67,7 +67,6 @@ public final class Reboot extends JavaPlugin {
                     sender.sendMessage(Reboot.config.getString("message.cancel_message"));
                 }
             }
-
         }
         return true;
     }
