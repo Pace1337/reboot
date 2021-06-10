e9ad1dd5c8c7ab45fab9d0a0836cd0022f980154
Pace <pacex3123@gmail.com>
2021-06-10 17:22:45 +0200

    Quick fix

--- src/main/java/dev/pace/reboot/Reboot.java
+++ src/main/java/dev/pace/reboot/Reboot.java
@@ -8,10 +8,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ResourceBundle;
+
 public final class Reboot extends JavaPlugin {
     public int rebootTaskID = 0;
     public boolean isRebooting = false;
-    public FileConfiguration config;
+    public static FileConfiguration config;
 
     @Override
     public void onEnable() {
